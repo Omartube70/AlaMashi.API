6852f57be803053c577593ffcbda29936a2e9c66
Omartube70 <omartubefaheen@gmail.com>
2025-08-08 05:47:02 +0300

    .

--- AlaMashi.API/Program.cs
+++ AlaMashi.API/Program.cs
@@ -1,29 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); // يمكن أن يظل هنا أو يتم نقله
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Configuration for Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
 
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+// Rest of the code...
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
-
-
-
 app.Run();
