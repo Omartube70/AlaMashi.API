5e182b6105efb6d304d581bc041ffb62fd99f56d
Omartube70 <omartubefaheen@gmail.com>
2025-08-10 03:04:12 +0300

    Update Connection String

--- AlaMashi.API/Program.cs
+++ AlaMashi.API/Program.cs
@@ -1,4 +1,6 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.Data.SqlClient; // تأكد من إضافة هذه الحزمة
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
--- AlaMashi.DAL/Settings.cs
+++ AlaMashi.DAL/Settings.cs
@@ -8,6 +8,6 @@ namespace AlaMashi.DAL
 {
     public class Settings
     {
-        public static string ConnectionString = "Server=.;Database=AlaMashiDB;Trusted_Connection=True;";
+        public static string ConnectionString = "Server=alamashidb.cxm2w8wqsgI9.eu-north-1.rds.amazonaws.com,1433;Database=AlaMashiDB;User Id=AlaMashiDB_User;Password=AlaMashiDB_Passowrd;TrustServerCertificate=True;";
     }
 }
